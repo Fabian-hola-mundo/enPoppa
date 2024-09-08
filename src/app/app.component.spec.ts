6244e17fb92eed06f81259cf15c8150072f8b3bf
Fabián Miranda <fabian.miranda@opitech.com.co>
2024-09-08 15:43:35 -0500

    start

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'enPoppa' title`, () => {
+  it(`should have the 'enpoppa' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('enPoppa');
+    expect(app.title).toEqual('enpoppa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, enPoppa');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, enpoppa');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"enpoppa2024","appId":"1:606636089590:web:286505bb855ac5f06b4cd7","storageBucket":"enpoppa2024.appspot.com","apiKey":"AIzaSyATLq70FvDXcNuEcw8j0A4it-sOYygRR0U","authDomain":"enpoppa2024.firebaseapp.com","messagingSenderId":"606636089590"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore())]
 };
